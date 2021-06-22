c8a47ba888830e6e52d953e28e757de47cc307f4
Etienne <etinaude@gmail.com>
2021-06-23 09:54:53 +1200

    accordian

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'front-end';
+  trainings = [
+    {
+      title: 'viynl',
+      time: '3pm',
+      complete: false,
+      users: [
+        { name: 'a', id: 1234, upi: 'abc123', attended: true },
+        { name: 'b', id: 1234, upi: 'abc123', attended: true },
+      ],
+    },
+    {
+      title: 'Lasers',
+      time: '3pm',
+      complete: false,
+      users: [
+        { name: 'a', id: 1234, upi: 'abc123', attended: true },
+        { name: 'b', id: 1234, upi: 'abc123', attended: true },
+      ],
+    },
+  ];
 }
