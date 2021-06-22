622abbb1f57d0b4ea87e783630bd0cfea22d663e
Etienne <etinaude@gmail.com>
2021-06-23 09:49:53 +1200

    materials

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from './materials/materials.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,14 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
