812f356b7d7c1e12fc61c3cfa4dbadeab69ed81e
Etienne <etinaude@gmail.com>
2021-06-23 16:17:07 +1200

    insert data into sheet

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,21 +1,43 @@
 import { eventbriteEvent } from './classes';
 import { ApiService } from './api.service';
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterContentInit {
   title = 'front-end';
   trainings: any = [];
+  selectedFacilitator = 'FRED!';
+  selectedEvent = { title: 'router' };
 
   constructor(public api: ApiService) {}
 
-  async ngOnInit() {
-    this.trainings = await this.api.getTodaysEvents();
+  async ngAfterContentInit() {
+    // this.trainings = await this.api.getTodaysEvents();
+    // console.log(this.trainings);
+  }
 
-    console.log(this.trainings);
+  login() {
+    this.api.signIn();
+  }
+
+  submitData() {
+    var table = '';
+
+    if (this.selectedEvent.title.includes('router')) {
+      table = 'CNC Router';
+    }
+
+    const body = {
+      values: [
+        ['1/1/1', 768485633, this.selectedFacilitator],
+        ['1/1/1', 12345, this.selectedFacilitator],
+      ],
+    };
+
+    this.api.insertData(body, table);
   }
 }
