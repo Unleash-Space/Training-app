f19abc2cda1dee21f6e9a2c7b0d2289b622153f6
htmoor <htmoor@gmail.com>
2021-06-23 16:49:16 +1200

    changes

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfirmComponent } from './confirm/confirm.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ConfirmComponent],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
