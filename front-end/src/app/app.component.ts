b512431bb9456c3ed0c4ffae70e805b0d1eada9f
htmoor <htmoor@gmail.com>
2021-06-23 13:53:22 +1200

    front end base

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'front-end';
   trainings = [
     {
-      title: 'viynl',
+      title: '23 June 2021 / 9AM / Viny cutter practice',
       time: '3pm',
       complete: false,
       users: [
@@ -18,13 +18,39 @@ export class AppComponent {
       ],
     },
     {
-      title: 'Lasers',
+      title: '23 June 2021 / 9AM / Sewing machine practice',
       time: '3pm',
       complete: false,
       users: [
         { name: 'a', id: 1234, upi: 'abc123', attended: true },
         { name: 'b', id: 1234, upi: 'abc123', attended: true },
       ],
+    },
+    {
+      title: '23 June 2021 / 9:30AM / Laser cutter practice',
+      time: '3pm',
+      complete: false,
+      users: [
+        { name: 'a', id: 1234, upi: 'abc123', attended: true },
+        { name: 'b', id: 1234, upi: 'abc123', attended: true },
+      ],
+    },
+  ];
+  attendees = [
+    {
+      name: 'Hayden Moore',
+      upi: 'hmoo908',
+      id: '8169070',
+    },
+    {
+      name: 'Etienne Naude',
+      upi: 'enaud123',
+      id: '123456789',
+    },
+    {
+      name: 'Test Student',
+      upi: 'tstud666',
+      id: '666666666',
     },
   ];
 }
