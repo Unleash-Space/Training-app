dd2f8f0f64bbd3bbc9f2e60bd07b3e6eb140f307
Etienne <etinaude@gmail.com>
2021-06-28 12:39:57 +1200

    validation

--- front-end/src/app/classes.ts
+++ front-end/src/app/classes.ts
@@ -2,19 +2,23 @@ export class eventbriteEvent {
   capacity?: number;
   description: any;
   summary?: string;
-  id: string;
-  start: { timezone: String; local: String; utc: String };
+  id: string = '';
+  start: { timezone: string; local: string; utc: string } = {
+    timezone: '',
+    local: '',
+    utc: '',
+  };
   attendees?: attendee[];
+  date: { date: string; time: string } = { date: '', time: '' };
+  name?: any;
+  title: string = '';
 
-  constructor() {
-    this.start = { timezone: '', local: '', utc: '' };
-    this.id = '';
-  }
+  constructor() {}
 }
 
 export class attendee {
   name?: string;
-  id?: string;
+  id: string = '';
   upi?: string;
   email?: string;
   attending?: boolean;
