5302bd0671971ca0c6ee9ffcf88655a930b25318
Etienne <eti@naude.dev>
2022-07-16 00:24:45 +1200

    fixing compilation

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { KEYS } from './keys';
+import KEYS from './keys.json';
 import { attendee, eventbriteEvent } from './classes';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
