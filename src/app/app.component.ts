be9e9c8e18b4d46b15624c3a2d4446e13744fbab
Etienne <eti@naude.dev>
2022-07-28 11:28:50 +1200

    trim spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -101,7 +101,9 @@ export class AppComponent implements AfterContentInit {
 
     this.selectedEvent.attendees.forEach((attendee: any) => {
       if (!attendee.attending) return;
-      if (attendee.attending) anyone_attending = true;
+      else anyone_attending = true;
+      this.trimSpaces(attendee);
+
       if (!this.validId(attendee.id)) valid = false;
       if (!this.validUpi(attendee.upi)) valid = false;
       if (!this.validEmail(attendee.email)) valid = false;
@@ -127,6 +129,14 @@ export class AppComponent implements AfterContentInit {
     } else {
       this.showConfirm = true;
     }
+  }
+
+  async trimSpaces(attendee: attendee) {
+    attendee.email = attendee.email.trim();
+    attendee.id = attendee.id.trim();
+    attendee.upi = attendee.upi.trim();
+    attendee.firstName = attendee.firstName.trim();
+    attendee.lastName = attendee.lastName.trim();
   }
 
   async submitData() {
@@ -171,22 +181,30 @@ export class AppComponent implements AfterContentInit {
   }
 
   public validId(id: string) {
+    id = id.trim();
+
     const regExp = /^(\d{7}|\d{9})$/;
 
     return id.match(regExp) !== null;
   }
 
   public validUpi(upi: string) {
+    upi = upi.trim();
+
     const regExp = /^[a-zA-Z]{1,4}\d{3}$/;
     return upi.match(regExp) !== null;
   }
 
   public validEmail(email: string) {
+    email = email.trim();
+
     const regExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return email.match(regExp) !== null;
   }
 
   public validName(name: string) {
+    name = name.trim();
+
     const regExp = /..+/;
     return name.match(regExp) !== null;
   }
