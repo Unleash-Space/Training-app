cc95329b9bcb1387cbcd54ebced56737c7691a1a
Etienne Naude <eti@naude.dev>
2023-08-03 10:54:44 +1200

    animations

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,11 +6,30 @@ import {
   ViewChild,
 } from '@angular/core';
 import { Banner, State } from '../classes';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('createDestroy', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateY(100%)' }),
+        animate(100),
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'translateY(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class BannerComponent {
   @Input() state!: State;
